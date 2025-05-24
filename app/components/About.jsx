a4e044d04b9d177cd2d3eb5ccb1615b711ffa39b
naman-omar <namanknp13@gmail.com>
2025-05-24 19:11:32 +0530

    updated About section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,18 +6,15 @@ import Image from 'next/image'
 import { motion } from "motion/react"
 
 const About = () => {
-
-    // taking out is darkmode
     const { isDarkMode, setIsDarkMode } = useTheme();
-
 
     return (
         <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1 }}
-
-            id='about' className='w-full max-w-full py-10 px-4 sm:px-8 md:px-[12%] scroll-mt-20'>
+            id='about'
+            className='w-full max-w-full py-10 px-4 sm:px-8 md:px-[12%] scroll-mt-20'>
 
             <motion.h4
                 initial={{ opacity: 0, y: -20 }}
@@ -35,7 +32,8 @@ const About = () => {
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 0.8 }}
-                className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+                className='flex w-full flex-col lg:flex-row items-center justify-center gap-16 min-h-[70vh] my-10'>
+
                 <motion.div
                     initial={{ opacity: 0, scale: 0.9 }}
                     whileInView={{ opacity: 1, scale: 1 }}
@@ -43,13 +41,16 @@ const About = () => {
                     className='w-64 sm:w-80 rounded-3xl max-w-none'>
                     <Image src={assets.profile_img} alt='' className='w-full rounded-3xl' />
                 </motion.div>
+
                 <motion.div
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 0.6, delay: 0.8 }}
-                    className='flex-1 '>
-                    <p className='mb-10 max-w-2xl font-ovo'>
-                    I am a passionate software developer with a solid foundation in data structures, algorithms, and full-stack development. I enjoy crafting efficient and scalable solutions, continuously exploring the latest technologies. . Through hands-on experience in projects and real-world applications, I bridge the gap between conceptual knowledge and implementation. Curiosity, problem-solving, and a drive for continuous learning fuel my growth as a developer. I thrive on tackling challenges and building meaningful software that delivers real impact.</p>
+                    className='flex-1'>
+                    <p className='mb-6 max-w-2xl font-ovo'>
+                        I'm a passionate software developer with strong skills in data structures, algorithms, and full-stack development. I enjoy building efficient, scalable solutions and thrive on solving real-world problems through continuous learning and hands-on experience.
+                    </p>
+
                     <motion.ul
                         initial={{ opacity: 0 }}
                         whileInView={{ opacity: 1 }}
@@ -58,20 +59,23 @@ const About = () => {
                         {infoList.map(({ icon, iconDark, title, description }, index) => (
                             <motion.li
                                 whileHover={{ scale: 1.05 }}
-                                className={`border -[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer  hover:-translate-y-1 duration-500
-                             ${isDarkMode ? "border-white hover:shadow-[4px_4px_0_#fff] hover:bg-[#2a004a]" : "hover:bg-[#fcf4ff] hover:shadow-[4px_4px_0_#000] "}`}
+                                className={`border -[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:-translate-y-1 duration-500
+                                ${isDarkMode ? "border-white hover:shadow-[4px_4px_0_#fff] hover:bg-[#2a004a]" : "hover:bg-[#fcf4ff] hover:shadow-[4px_4px_0_#000]"}`}
                                 key={index}>
-                                <Image src={isDarkMode ? iconDark : icon} alt={title} className={`w-7 mt-3 `} />
+                                <Image src={isDarkMode ? iconDark : icon} alt={title} className='w-7 mt-3' />
                                 <h3 className={`my-4 font-semibold text-gray-700 ${isDarkMode ? "text-white" : ""}`}>{title}</h3>
                                 <p className={`text-gray-600 text-sm ${isDarkMode ? "text-white/80" : ""}`}>{description}</p>
                             </motion.li>
                         ))}
                     </motion.ul>
+
                     <motion.h4
                         initial={{ opacity: 0, y: 20 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.5, delay: 1.3 }}
-                        className={`my-6 text-gray-700 font-ovo ${isDarkMode ? "text-white/80" : ""}`}>Tools I use</motion.h4>
+                        className={`my-6 text-gray-700 font-ovo ${isDarkMode ? "text-white/80" : ""}`}>
+                        Tools I use
+                    </motion.h4>
 
                     <motion.ul
                         initial={{ opacity: 0 }}
@@ -80,8 +84,8 @@ const About = () => {
                         className='flex items-center gap-3 sm:gap-5'>
                         {toolsData.map((tool, index) => (
                             <motion.li
-                                whileHover={{scale:1.1}}
-                                className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 '
+                                whileHover={{ scale: 1.1 }}
+                                className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500'
                                 key={index}>
                                 <Image src={tool} alt='Tool' className='w-5 sm:w-7' />
                             </motion.li>
@@ -90,7 +94,7 @@ const About = () => {
                 </motion.div>
             </motion.div>
         </motion.div>
-    )
-}
+    );
+};
 
-export default About
+export default About;
